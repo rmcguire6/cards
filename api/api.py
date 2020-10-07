eb650d69df8bbc713a13105564620d6af2db769c
Regina McGuire <rmcguire@antelecom.net>
2020-10-07 14:57:57 -0700

    Create api.py with database

---
+++ api/api.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from flask_marshmallow import Marshmallow
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'matches.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/')
+def homepage():
+    return ('homepage')
+
+# cli commands
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
+
+if __name__ == '__main__':
+    app.run()
+
+ 
