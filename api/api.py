baca6ea85cee3446ba24979d71cc279f673899dc
Regina McGuire <rmcguire@antelecom.net>
2020-10-12 18:50:39 -0700

    Add database seed and drop scripts

--- api/api.py
+++ api/api.py
@@ -58,6 +58,43 @@ def db_create():
     db.create_all()
     print('Database created!')
 
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database dropped')
+
+@app.cli.command('db_seed')
+def db_seed():
+    vivir = Spanish(word = 'vivir',
+                    part='verb',
+                    group='ir')
+
+    comer = Spanish(word = 'comer',
+                    part='verb',
+                    group='er')
+
+    hablar = Spanish(word = 'hablar',
+                    part='verb',
+                    group='ar')
+
+    db.session.add(vivir)
+    db.session.add(comer)
+    db.session.add(hablar)
+
+    live = English(english_word ='live',
+                    spanish_word = 'vivir')
+
+    eat = English(english_word ='eat',
+                    spanish_word = 'comer')
+
+    speak = English(english_word ='speak',
+                    spanish_word = 'hablar')
+
+    db.session.add(live)
+    db.session.add(eat)
+    db.session.add(speak)
+    db.session.commit()
+    print('Database seeded!')
 
 if __name__ == '__main__':
     app.run()
