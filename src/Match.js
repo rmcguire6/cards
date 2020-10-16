0bfcbba8d12c1d011936d7b69ccd2fed0a3c850d
Regina McGuire <rmcguire@antelecom.net>
2020-10-16 16:22:25 -0700

    Add word match logic

--- src/Match.js
+++ src/Match.js
@@ -6,12 +6,18 @@ const Match = () => {
   const {spanishWords, englishWords} = useContext(MatchesContext)
   const [displaySpanishWords, setDisplaySpanishWords] = useState([])
   const [displayEnglishWords, setDisplayEnglishWords] = useState([])
- 
+  const [selectedEnglishWord, setSelectedEnglishWord] = useState('')
+  const [selectedSpanishWord, setSelectedSpanishWord] = useState('')
   useEffect(() => {
         setDisplayEnglishWords(createRandomList(englishWords))
         setDisplaySpanishWords(spanishWords)
   }, [englishWords, spanishWords])
-  
+  const handleEnglishClick =(item) => {
+    setSelectedEnglishWord(item)
+  }
+  const handleSpanishClick =(item) => {
+    setSelectedSpanishWord(item)
+  }
   return (
     <div className='container'>
       <h2>Match</h2>
@@ -19,18 +25,19 @@ const Match = () => {
         <div className='list'>
             {displaySpanishWords.map(spanish => (
                 <div className="card" key={spanish.spanish_id}>
-                  <button className="button" >{spanish.word}</button>
+                  <button className="button" onClick={() => handleSpanishClick(spanish.word)}>{spanish.word}</button>
                 </div>
             ))}
         </div>
         <div className='list'>
             {displayEnglishWords.map(english => (
                 <div className="card" key={english.english_id}>
-                  <button className="button" >{` to ${english.english_word}`}</button>
+                  <button className="button" onClick={() => handleEnglishClick(english.spanish_match)}>{` to ${english.english_word}`}</button>
                 </div>
             ))}
         </div>
       </div>
+      {selectedSpanishWord !== '' && selectedSpanishWord === selectedEnglishWord ? <p>Words Match</p> : <p>Words Don't Match</p>}
     </div>
   )
 }
