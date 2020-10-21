11ba1f64cae95acab9184e28fc5f3f2f12310f23
Regina McGuire <rmcguire@antelecom.net>
2020-10-20 21:02:59 -0700

    Add form to AddMatches

--- src/AddMatches.js
+++ src/AddMatches.js
@@ -1,6 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
-const AddMatches = () => (
-    <h3>Add a Word Match</h3>
-)
-export {AddMatches as default}
+const AddMatches = () => {
+  const [english, setEnglish] = useState('')
+  const [spanish, setSpanish] = useState('')
+  const [selectedPartOfSpeech, setSelectedPartOfSpeech] = useState('verb')
+
+  const addMatch = (e) => {
+    e.preventDefault()
+    const spanishWord = {word: spanish, part: selectedPartOfSpeech, group: '', english_match: english }
+    const englishWord = {english: english, spanish_match: spanish}
+    console.log('spanish', spanishWord, 'english', englishWord)
+    setSpanish('')
+    setEnglish('')
+  }
+
+  return (
+    <>
+      <h3>Add a Word Match</h3>
+      <form onSubmit={addMatch}>
+        <div className='form'>
+          <div className='form_input container'>
+            <label htmlFor='spanish' className="bold label">Spanish</label>
+            <input name='spanish' value={spanish} onChange={(e) => setSpanish(e.target.value)} />
+            <p className='bold'>Select a part of speech</p>
+            <div>
+                <label>
+                <input
+                  type='radio'
+                  name='part'
+                  value='verb'
+                  checked={selectedPartOfSpeech === 'verb'}
+                  onChange={(e) => setSelectedPartOfSpeech(e.target.value)}
+                />
+                Verb
+                </label>
+            </div>
+            <div>
+                <label>
+                <input
+                   type='radio'
+                   name='part'
+                   value='noun'
+                   checked={selectedPartOfSpeech === 'noun'}
+                   onChange={(e) => setSelectedPartOfSpeech(e.target.value)}
+                />
+                Noun
+                </label>
+            </div>
+                <label>
+                <input
+                  type='radio'
+                  name='part'
+                  value='other'
+                  checked={selectedPartOfSpeech === 'other'}
+                  onChange={(e) => setSelectedPartOfSpeech(e.target.value)}
+                />
+                Other
+                </label>
+            </div>
+          </div>
+          <div className='form_input container'>
+            <label htmlFor='english' className="bold label">English</label>
+            <input name='english' value={english} onChange={(e) => setEnglish(e.target.value)} />
+          </div>
+        <button className='button'>Add a word pair</button>
+      </form>
+    </>
+  )
+}
+export { AddMatches as default }
+
