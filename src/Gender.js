638dcc63e695c165a2a9afb3256e362df5bd4a83
Regina McGuire <rmcguire@antelecom.net>
2020-11-02 20:13:42 -0800

    Refactor Gender to clearly show gender choice

--- src/Gender.js
+++ src/Gender.js
@@ -1,28 +1,43 @@
-import React, { useState } from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import Results from './Results'
+import MatchesContext from './matches-context'
 import './App.css'
 
-const Gender = () => {
-  const [answer, setAnswer] = useState('')
-  const spanish = {
-    spanish: 'pelota',
-    part: 'noun',
-    group: 'fem'
-  }
 
-  const handleGenderClick =(gender) => {
+const Gender = () => {
+  const {spanishWords} = useContext(MatchesContext)
+  const [spanishList, setSpanishList] = useState([])
+  const [selectedSpanishWord, setSelectedSpanishWord] = useState('')
+  const [answer, setAnswer] = useState('')
+
+  useEffect(() => {
+    setSpanishList(spanishWords.filter((word) => word.part === 'noun'))
+  }, [spanishWords])
+
+  const handleGenderClick =(gender, spanish) => {
+    setSelectedSpanishWord(spanish)
     setAnswer(gender)
+    console.log('spanish part', selectedSpanishWord, 'answer', answer)
+
   }
   return (
     <div className='container'>
       <div>
         <h2 >What's the Gender?</h2>
-        <h3>Click the Correct One</h3>
+        {spanishList.length > 0? <h3>Click El or La</h3> : <h3>Add Nouns to Matches</h3>}
         <div className='container'>
           <div className='gender'>
-            <button className='button'onClick={() => handleGenderClick('masc')}>El {spanish.spanish}</button><button className='button'onClick={() => handleGenderClick('fem')}>La {spanish.spanish}</button>
+            <div className='list'>
+            {spanishList.map(spanish => (
+                <div className="card" key={spanish.spanish_id}>
+                  <div className='line'>
+                    <button className="button" onClick={() => handleGenderClick('masc', spanish)}>El</button><button className='button'onClick={() => handleGenderClick('fem', spanish)}>La</button><p className='button'>{spanish.spanish}</p>
+                  </div>
+                </div>
+            ))}
+            </div>
           </div>
-          {answer.length === 0 ? <p className='button'>No Word Selected</p> :  <Results spanishWord={spanish.group} englishWord={answer}/>}
+          {answer.length === 0 ? <p className='button'>No Word Selected</p> :  <Results spanishWord={selectedSpanishWord.group} englishWord={answer}/>}
         </div>
       </div>
     </div>
