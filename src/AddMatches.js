d696ef0f01bcd8c639e026370b1a754684b294bd
Regina McGuire <rmcguire@antelecom.net>
2021-01-22 17:10:43 -0800

    Add error message to AddMatch

--- src/AddMatches.js
+++ src/AddMatches.js
@@ -6,26 +6,42 @@ const AddMatches = () => {
   const [spanish, setSpanish] = useState('')
   const selectedPartOfSpeech = 'verb'
   const group = ''
+  const [error, setError] = useState('')
+
   const addMatch = (e) => {
     e.preventDefault()
+    if (!spanish || !english) {
+      setError('Please provide both Spanish and English words')
+    } else {
+      setError('')
     sendMatch({spanish:spanish, english:english, part: selectedPartOfSpeech, group: group, spanish_match: spanish, english_match: english})
     setSpanish('')
     setEnglish('')
+    }
   }
 
   return (
     <>
       <h3>Add a Word Match</h3>
+      {error && <p className='display'>{error}</p>}
       <form onSubmit={addMatch}>
         <div className='container'>
           <label htmlFor='spanish' className="label">Spanish</label>
-          <input name='spanish' value={spanish} onChange={(e) => setSpanish(e.target.value)} />
+          <input
+            name='spanish'
+            value={spanish}
+            onChange={(e) => setSpanish(e.target.value)}
+            autoFocus
+            />
         </div>
         <div className='container'>
           <label htmlFor='english' className="label">English</label>
-          <input name='english' value={english} onChange={(e) => setEnglish(e.target.value)} />
+          <input
+            name='english'
+            value={english}
+            onChange={(e) => setEnglish(e.target.value)} />
         </div>
-        <button className='button'>Add a word pair</button>
+        <button className='button'>Add a matching word pair</button>
       </form>
     </>
   )
